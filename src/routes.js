58410938566f6eec97af0c0c77fd186e082b0e01
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-03 15:32:18 +0300

    Create movie page

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 
 
 const homeController = require('./controllers/homeController');
+const createController = require('./controllers/createController');
 router.use(homeController);
-
+router.use(createController)
 module.exports = router;
