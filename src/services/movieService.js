6d56ae6545bb8846356a6c9184f887e968fcf78b
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-03 17:05:49 +0300

    Add movie id's

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,10 +1,21 @@
-const movies = [];
+const movies = [
+    {   _id: 1,
+        title: 'vbdbd',
+        genre: '',
+        director: '',
+        year: '',
+        imageURL: '',
+        rating: '',
+        description: ''
+      }
+];
 
 
 exports.getAll = () => {
     return movies.slice();
 }
 exports.create = (movieData) => {
+    movieData._id = movies[movies.length - 1]._id +  1;
     movies.push(movieData)
-    console.log(movies)
+    
 }
