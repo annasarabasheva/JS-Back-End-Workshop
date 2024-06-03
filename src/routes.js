bbeb79366726a01991dc2f34530173958c4d56f9
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-03 15:38:21 +0300

    Render 404 ERROR

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,10 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
 router.use(homeController);
-router.use(createController)
+router.use(createController);
+
+
+router.all('*', (req, res) => {
+    res.render('404');
+})
 module.exports = router;
