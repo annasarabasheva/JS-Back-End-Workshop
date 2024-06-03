dc5c62b27e95f60e8e6a1f24f2c2029ec402ec23
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-03 13:29:39 +0300

    Add express-handlebars

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
+const handlebars = require('express-handlebars');
+const path = require('path');
+
+
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', path.join(__dirname, 'views'))
+
 
 app.get('/', (req, res) => {
-    res.send('Hey there')
+    res.render('home', { layout: false });
+  
 })
 
 
