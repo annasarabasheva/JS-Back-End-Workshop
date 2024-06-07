f1964474d1407a2f3ec5fe7bbc75e6212966e11b
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-07 14:43:29 +0300

    Show details page correctly

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -17,9 +17,9 @@ router.post('/create', async (req, res) => {
     
 })
 
-router.get('/details/:movieId', (req, res) => {
+router.get('/details/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
-    const movie = movieService.getOne(movieId);
+    const movie = await movieService.getOne(movieId).lean();
     res.render('details', {movie});
 });
 
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,21 +1,8 @@
 const Movie = require('../models/Movie')
 
-
-
-const movies = [
-    {   _id: 1,
-        title: 'vbdbd',
-        genre: '',
-        director: '',
-        year: '',
-        imageURL: '',
-        rating: '',
-        description: ''
-      }
-];
-
 exports.getOne = (movieId) => {
-    return movies.find(movie => movie._id == movieId);
+    const movie = Movie.findById(movieId);
+    return movie;
 }
-exports.getAll =  () => {  return  Movie.find() }
-exports.create = async (movieData) => { return await Movie.create(movieData); }
+exports.getAll =  () => Movie.find();
+exports.create = (movieData) => Movie.create(movieData); 
