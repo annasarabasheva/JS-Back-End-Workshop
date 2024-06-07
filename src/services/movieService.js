a4727defa1a83bb352de0251093b345b0f049f99
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-07 18:45:26 +0300

    Populating casts for specific movie

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -1,5 +1,5 @@
 const Movie = require('../models/Movie')
 
-exports.getOne = (movieId) =>  Movie.findById(movieId);
+exports.getOne = (movieId) =>  Movie.findById(movieId).populate('casts');
 exports.getAll =  () => Movie.find();
 exports.create = (movieData) => Movie.create(movieData); 
