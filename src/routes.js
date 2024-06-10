9a987f2279ab7165a1320364d3b022a488e59ad8
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-10 13:45:46 +0300

    Add register page

--- src/routes.js
+++ src/routes.js
@@ -4,10 +4,12 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
 const castController = require('./controllers/castController');
+const authController = require('./controllers/authController')
 
 router.use(homeController);
 router.use(createController);
 router.use(castController);
+router.use(authController);
 
 
 router.all('*', (req, res) => {
