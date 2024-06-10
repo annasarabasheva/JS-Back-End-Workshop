c71f5b35a2b1112eac9de7154f56cf83ea2e31cb
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-10 14:07:42 +0300

    Creating user model and get the data inside the db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,16 @@
 const router = require('express').Router();
+const authService = require('../services/authService');
 
 router.get('/register', (req, res) => {
     res.render('register')
 })
 
 
+router.post('/register', async (req, res) => {
+    const userData = req.body;
+    await authService.register(userData);
+    res.redirect('/login')
+
+})
 
 module.exports = router;
--- src/routes.js
+++ src/routes.js
@@ -4,7 +4,7 @@ const router = require('express').Router();
 const homeController = require('./controllers/homeController');
 const createController = require('./controllers/createController');
 const castController = require('./controllers/castController');
-const authController = require('./controllers/authController')
+const authController = require('./controllers/authController');
 
 router.use(homeController);
 router.use(createController);
