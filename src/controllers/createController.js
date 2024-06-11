386a8b43570cdd7e04d9963bdaabb0160d0024c7
Anna Sarabasheva <annasarabasheva@gmail.com>
2024-06-11 15:06:42 +0300

    Implement logic for delete button

--- src/controllers/createController.js
+++ src/controllers/createController.js
@@ -27,7 +27,7 @@ router.post('/create', isAuth, async (req, res) => {
 router.get('/details/:movieId', async (req, res) => {
     const movieId = req.params.movieId;
     const movie = await movieService.getOne(movieId).lean();
-    const isOwner = movie.owner == req.user._id;
+    const isOwner = movie.owner == req.user?._id;
 
     res.render('details', {movie, isOwner});
 });
@@ -58,6 +58,10 @@ router.get('/edit/:movieId', isAuth, async (req, res) => {
 });
 
 
+router.get('/delete/:movieId', isAuth, async (req, res) => {
+    await movieService.delete(req.params.movieId);
+    res.redirect('/')
+});
 
 
 module.exports = router;
--- src/services/movieService.js
+++ src/services/movieService.js
@@ -3,3 +3,4 @@ const Movie = require('../models/Movie')
 exports.getOne = (movieId) =>  Movie.findById(movieId).populate('casts');
 exports.getAll =  () => Movie.find();
 exports.create = (movieData) => Movie.create(movieData); 
+exports.delete = (movieId) => Movie.findByIdAndDelete(movieId);
